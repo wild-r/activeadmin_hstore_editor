8952a31ff81cdbddb5b6d577d650ffa91270bd34
varun jayaraman <varun.jayaraman@gmail.com>
2018-03-22 15:54:06 +0100

    use function expression not a function declaration

--- app/assets/javascripts/active_admin/hstore_editor.js
+++ app/assets/javascripts/active_admin/hstore_editor.js
@@ -3,22 +3,21 @@
 
 ;(function(window, $) {
   $(document).ready(function() {
+    var createEditor = function() {
+      $('div.jsoneditor-wrap').each(function(i,wrap){
+        var container = $(wrap)[0];
+        var textarea = $($(wrap).find('textarea'));
+        var editor;
+        var options = {
+          mode: 'tree',
+          change: function(ev){
+            textarea.text(JSON.stringify(editor.get()));
+          }
+        };
+        editor = new JSONEditor(container, options,JSON.parse(textarea.val()));
+      });
+    }
     createEditor();
     $(document).on('page:load turbolinks:load', createEditor);
   });
-
-  function createEditor() {
-    $('div.jsoneditor-wrap').each(function(i,wrap){
-      var container = $(wrap)[0];
-      var textarea = $($(wrap).find('textarea'));
-      var editor;
-      var options = {
-        mode: 'tree',
-        change: function(ev){
-          textarea.text(JSON.stringify(editor.get()));
-        }
-      };
-      editor = new JSONEditor(container, options,JSON.parse(textarea.val()));
-    });
-  }
 })(window, jQuery);
